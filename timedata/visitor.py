86f615bb7f20ee882ce77bf7813f0da8d4be031e
Tom Ritchford <tom@swirly.com>
2018-09-05 17:34:43 +0200

    Restrict to 79 columns

--- timedata/visitor.py
+++ timedata/visitor.py
@@ -15,13 +15,14 @@ def no_visit(value, key, parent):
     pass
 
 
-def visit(project, post=no_visit, pre=no_visit, children=simple_children, **kw):
+def visit(project, post=no_visit, pre=no_visit, children=simple_children,
+          **kwds):
     def recurse(value, key, parent):
-        pre(value, key, parent, **kw)
+        pre(value, key, parent, **kwds)
 
         for child_key, child_value in list(children(value)):
             recurse(child_value, child_key, value)
 
-        post(value, key, parent, **kw)
+        post(value, key, parent, **kwds)
 
     recurse(project, '', None)
