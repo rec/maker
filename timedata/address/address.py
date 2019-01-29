17866263e771eaa33e1d84fb18ebb43767f721f3
Tom Ritchford <tom@swirly.com>
2019-01-29 18:35:38 +0100

    Split off address/generate.py

--- timedata/address/address.py
+++ timedata/address/address.py
@@ -29,7 +29,7 @@ convenient but prevents the creation of dictionaries like
 """
 
 import json
-from . import segment
+from . import generate, segment
 
 
 class Address:
@@ -44,7 +44,7 @@ class Address:
         self.name = self.name.strip()
 
         try:
-            self.segments = list(_generate(self.name))
+            self.segments = list(generate.generate(self.name))
         except:
             raise ValueError('%s is not a legal address' % name)
 
@@ -85,67 +85,3 @@ class Address:
         *first, last = self.segments
         parent = self._get(root, first)
         last.set(parent, *(self.assignment + values))
-
-
-def _generate(s):
-    def extract_calls(p):
-        # Extract () pairs from start and finish of a string
-        before, after = [], []
-        while p.startswith('()'):
-            before.append(segment.Call())
-            p = p[2:]
-
-        while p.endswith('()'):
-            after.append(segment.Call())
-            p = p[:-2]
-
-        return before, p, after
-
-    # Split on dots, then use [ and ] to split out indices
-    s = s.strip()
-    if s.startswith('.'):
-        s = s[1:]
-    if s.endswith('.'):
-        raise ValueError
-
-    for i, part in enumerate(s.split('.')):
-        if not part:
-            raise ValueError
-
-        head, *rest = part.split('[')
-
-        # If we had e.g. 'xxx()()[yyy]()[zzz]()()'
-        # Now we have first='xxx()' and rest = 'yyy]()', 'zzz]()()'
-        # They might have written: ()xxx by mistake
-        before, head, after = extract_calls(head)
-
-        if before:
-            # A call () is only allowed to start the first segment -
-            # for example, an address like a.() is forbidden.
-            if i or head:
-                raise ValueError
-            yield from before
-
-        elif head:
-            yield segment.Attribute(head)
-            yield from after
-
-        elif i:
-            # An index [] is only allowed to start the first segment -
-            # for example, an address like a.[2] is forbidden.
-            raise ValueError
-
-        for r in rest:
-            before, r, after = extract_calls(r)
-            if before:
-                # A segment cannot contain a () - they must all be at top level
-                raise ValueError
-
-            # A segment must have exactly one ']', exactly at the end.
-            index, between = r.split(']', 1)
-            if between:
-                raise ValueError
-
-            index = int(index) if index.isdigit() else index
-            yield segment.Index(index)
-            yield from after
