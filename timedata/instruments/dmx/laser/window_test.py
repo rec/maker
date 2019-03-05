09822f7b7d09dd75b18fdcecc9ea801685486ff7
Tom Ritchford <tom@swirly.com>
2019-03-05 13:02:27 +0100

    Remove cruft in window_test.py

--- timedata/instruments/dmx/laser/window_test.py
+++ timedata/instruments/dmx/laser/window_test.py
@@ -4,99 +4,81 @@ from . import color_selector, constants, dmx_levels, abs_lfo_fader, one_laser
 from timedata.ui import bang, selector, toggle_button, notes_held
 
 
-class MyFirstGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title('laser recorder')
+def make_gui(master):
+    master.title('laser recorder')
 
-        if False:
-            self.laser = one_laser.OneLaser(master, 'A 1')
-            self.laser.pack(fill=tk.BOTH, expand=tk.YES)
+    if not False:
+        laser = one_laser.OneLaser(master, 'A 1')
+        laser.pack(fill=tk.BOTH, expand=tk.YES)
 
-        if False:
-            for i in range(5):
-                tk.Label(master, text=str(i)).grid(row=0, column=i)
+    if False:
+        for i in range(5):
+            tk.Label(master, text=str(i)).grid(row=0, column=i)
 
-            tk.Label(master, text='A').grid(row=1, column=0, columnspan=3)
+        tk.Label(master, text='A').grid(row=1, column=0, columnspan=3)
 
-        if False:
-            self.dmx_levels = dmx_levels.DMXLevels(self.master)
-            self.dmx_levels.pack(fill=tk.BOTH, expand=tk.YES)
+    if False:
+        dmx_levels = dmx_levels.DMXLevels(master)
+        dmx_levels.pack(fill=tk.BOTH, expand=tk.YES)
 
-        if False:
-            self.colors = color_selector.ColorSelector(master)
-            self.patterns = selector.Selector(master, constants.Patterns)
-            self.colors.pack()
-            self.patterns.pack()
+    if False:
+        colors = color_selector.ColorSelector(master)
+        patterns = selector.Selector(master, constants.Patterns)
+        colors.pack()
+        patterns.pack()
 
-            self.colors.add_callback(print)
-            self.patterns.add_callback(print)
+        colors.add_callback(print)
+        patterns.add_callback(print)
 
-        if False:
-            self.button = toggle_button.ToggleButton(master, 'X', 'O', print)
-            self.button.pack()
+    if False:
+        button = toggle_button.ToggleButton(master, 'X', 'O', print)
+        button.pack()
 
-        if not False:
-            self.alf = abs_lfo_fader.AbsLfoFader(master, 'TEST', print, print)
-            self.alf.pack()
+    if False:
+        alf = abs_lfo_fader.AbsLfoFader(master, 'TEST', print, print)
+        alf.pack()
 
-        if False:
-            import time
-            self.bang = bang.Bang(master, 'MIDI', off='yellow', on='green',
-                                  font=('Helvetica', 24))
-            self.bang.pack()
-            # print(self.bang.actual())
+    if False:
+        import time
+        bang = bang.Bang(master, 'MIDI', off='yellow', on='green',
+                              font=('Helvetica', 24))
+        bang.pack()
+        # print(bang.actual())
 
-            def b(t):
-                self.bang.bang()
-                time.sleep(t)
+        def b(t):
+            bang.bang()
+            time.sleep(t)
 
-            def target():
-                b(2)
-                b(2)
-                b(0.5)
-                b(0.5)
-                b(0.5)
-                b(3)
-                b(0.1)
-                b(0.1)
-                b(0.1)
-                b(0.1)
-                b(0.1)
-                b(3)
+        def target():
+            b(2)
+            b(2)
+            b(0.5)
+            b(0.5)
+            b(0.5)
+            b(3)
+            b(0.1)
+            b(0.1)
+            b(0.1)
+            b(0.1)
+            b(0.1)
+            b(3)
 
-            threading.Thread(target=target, daemon=True).start()
+        threading.Thread(target=target, daemon=True).start()
 
-        if False:
-            notes = notes_held.NotesHeld(master)
-            notes.pack()
-            notes.note_on(200, 'red')
-            notes.note_on(-1, 'red')
+    if False:
+        notes = notes_held.NotesHeld(master)
+        notes.pack()
+        notes.note_on(200, 'red')
+        notes.note_on(-1, 'red')
 
-            notes.note_on(0)
-            notes.note_on(20, 'green')
-            notes.note_on(127, 'green')
-            notes.note_on(20, 'green')
-            notes.note_on(64, 'blue')
-
-    def greet(self):
-        print("Greetings!")
-
-    def old(self):
-        master = self.master
-        self.label = ttk.Label(master, text="This is our first GUI!")
-        self.label.pack()
-
-        self.greet_button = ttk.Button(
-            master, text="Greet", command=self.greet)
-        self.greet_button.pack()
-
-        self.close_button = ttk.Button(
-            master, text="Close", command=master.quit)
-        self.close_button.pack()
+        notes.note_on(0)
+        notes.note_on(20, 'green')
+        notes.note_on(127, 'green')
+        notes.note_on(20, 'green')
+        notes.note_on(64, 'blue')
 
 
 if __name__ == '__main__':
     root = tk.Tk()
-    my_gui = MyFirstGUI(root)
+    make_gui(root)
     root.mainloop()
