622d121ae9ccd4d83bc7a12aa0217fc200b58814
Tom Ritchford <tom@swirly.com>
2019-03-13 17:23:01 +0100

    Port bang.py to kivy

--- timedata/ui/bang.py
+++ timedata/ui/bang.py
@@ -1,72 +1,46 @@
 import threading, time
 from kivy.uix.togglebutton import ToggleButton
-from kivy.uix.widget import Label
+from kivy.uix.label import Label
 from kivy import graphics
 
 STATES = 'normal', 'down'
 
 
-class BangOld(ToggleButton):
+class Bang(Label):
+    def on_size(self, *args):
+        self.canvas.before.clear()
+        if self._state:
+            with self.canvas.before:
+                graphics.Color(0, 1, 1)
+                graphics.Rectangle(pos=self.pos, size=self.size)
+
     def __init__(self, text, delay=0.25, **kwds):
         super().__init__(text=text, **kwds)
         self.delay = delay
         self.lock = threading.RLock()
         self._state = False
 
-    def bang(self):
-        with self.lock:
-            self.target_time = time.time() + self.delay
-            if not self._state:
-                self._state = True
-                self.state = 'down'
-                threading.Thread(target=self._target, daemon=True).start()
+    @property
+    def _state(self):
+        return self.state == STATES[1]
 
-    def _target(self):
-        while True:
-            with self.lock:
-                remains = self.target_time - time.time()
-                self._state = (remains > 0)
-                if not self._state:
-                    self.state = 'normal'
-                    break
-            time.sleep(remains)
-
-    def on_click(self):
-        # Disable clicking
-        pass
-
-
-class Bang(Label):
-    def on_size(self, *args):
-        self.canvas.before.clear()
-        with self.canvas.before:
-            graphics.Color(0, 1, 0, 0.25)
-            graphics.Rectangle(pos=self.pos, size=self.size)
-
-    def __init__(self, text, color, delay=0.25, **kwds):
-        super().__init__(text=text, **kwds)
-        self.delay = delay
-        self.lock = threading.RLock()
-        self._state = False
+    @_state.setter
+    def _state(self, state):
+        self.state = STATES[bool(state)]
+        self.on_size()
 
     def bang(self):
         with self.lock:
             self.target_time = time.time() + self.delay
             if not self._state:
                 self._state = True
-                self.state = 'down'
                 threading.Thread(target=self._target, daemon=True).start()
 
     def _target(self):
         while True:
             with self.lock:
                 remains = self.target_time - time.time()
-                self._state = (remains > 0)
-                if not self._state:
-                    self.state = 'normal'
+                if remains <= 0:
+                    self._state = False
                     break
             time.sleep(remains)
-
-    def on_click(self):
-        # Disable clicking
-        pass
