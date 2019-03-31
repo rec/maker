73170865f67ea4c0caf3be2ff89ae527ae92710a
Tom Ritchford <tom@swirly.com>
2019-03-31 17:48:10 +0200

    Fix test_all by disabling some modules

--- test/timedata/all_test.py
+++ test/timedata/all_test.py
@@ -2,16 +2,34 @@ import os, unittest
 from . import_all import import_all
 
 IS_TRAVIS = os.getenv('TRAVIS', '').lower().startswith('t')
-BLACKLIST = (['timedata.control.keyboard'] if IS_TRAVIS else []) + [
-    'timedata.instruments.dmx.laser.laser',
-    'timedata.ui.ui_test',
-    'timedata.instruments.dmx.laser.window_test',
-    'timedata.instruments.dmx.laser.window_test2']
-DONT_WARN = 'timedata.control.keyboard',
+BLACKLIST = [
+    'timedata.control.keyboard',
+    'timedata.instruments.laser.abs_lfo_fader',
+    'timedata.instruments.laser.dmx_levels',
+    'timedata.instruments.laser.laser',
+    'timedata.instruments.laser.loose_buttons',
+    'timedata.instruments.laser.main',
+    'timedata.instruments.laser.one_laser',
+    'timedata.instruments.laser.selectors',
+    'timedata.instruments.laser.wombat',
+    'timedata.instruments.old_laser.six_faders',
+    'timedata.instruments.old_laser.six_lasers',
+    'timedata.instruments.old_laser.top_window',
+    'timedata.ui.bang',
+    'timedata.ui.box_layout',
+    'timedata.ui.int_entry',
+    'timedata.ui.int_slider',
+    'timedata.ui.notes_held',
+    'timedata.ui.selector',
+    'timedata.ui.switch_button',
+    'timedata.ui.ui_test']
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 
 class ImportAllTest(unittest.TestCase):
-    def DONT_test_all(self):
-        _, fail = import_all(PROJECT_ROOT, 'timedata', BLACKLIST, DONT_WARN)
-        self.assertEqual(fail, [])
+    maxDiff = 5000
+
+    def test_all(self):
+        _, fail = import_all(PROJECT_ROOT, 'timedata', [])
+        fail = [m for (m, e) in fail]
+        self.assertEqual(fail, BLACKLIST)
